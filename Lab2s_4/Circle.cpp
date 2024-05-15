e3f625342359ec419e4dce2dbff7767bb8d9b927
Ershov Vladislav Romanovich <vrershov05@gmail.com>
2024-05-15 13:07:34 +0300

    Доделана лабораторная работа 4, вариант до правок(если будут после)

--- Lab2s_4/Circle.cpp
+++ Lab2s_4/Circle.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
 #include <string.h>
+#include <cmath>
+#define _USE_MATH_DEFINES
+#include <math.h>
 using namespace std;
 
 class Point {
 public:
 	Point(int x = 0, int y = 0) : x(x), y(y) {
-		cout << "Вызвался конструктор Point: "<< this << endl;
+		//cout << "Вызвался конструктор Point: "<< this << endl;
 	}
 
 
@@ -26,7 +29,7 @@ public:
 	}
 
 	~Point() {
-		cout << "Вызвался деструктор Point: " << this << endl;
+		//cout << "Вызвался деструктор Point: " << this << endl;
 	}
 
 
@@ -36,7 +39,8 @@ public:
 		return this->x == other.x and this->y == other.y;
 
 	}
-
+	friend ostream& operator <<(ostream&, const Point&);
+	friend istream& operator >>(istream& in, Point& p);
 	bool operator !=(const Point& other)
 	{
 		return x != other.x or y != other.y;
@@ -97,25 +101,148 @@ protected:
 };
 
 class Circle {
-public:
-	Circle(Point centr, double radius = 0){
-		this->centr = centr;
-		this->radius = radius;
-		cout << "Вызвался конструктор Circle: " << this << endl;
-	}
-	Circle() { 
-		this->radius = 0;
-		cout << "Вызвался базовый конструктор Circle: " << this << endl; 
-	}
-
-	void print() {
-		centr.print();
-		cout << "Радиус: " << radius << endl;
-	}
-	~Circle() {
-		cout << "Вызвался деструктор Circle: " << this << endl;
-	}
-private:
 	Point centr;
 	double radius;
+public:
+	Circle() :radius(0) {}
+	Circle(Point p, int r) :centr(p), radius(r) {}
+	bool operator==(const Circle& other) {
+		return ((radius == other.radius) && (centr == other.centr));
+	}
+	const double GetS()const {
+		return  M_PI * radius * radius;
+	}
+	friend ostream& operator <<(ostream&, const Circle&);
+	friend istream& operator >>(istream& in, Circle& c);
+};
+
+//Этот класс отвечает за создание и управление узлами в двусвязном списке.
+class Node {
+	Circle data;
+	Node* pNext;
+	Node* pPrev;
+
+	//Устанавливает указатели pNext и pPrev в nullptr, что обозначает, что узел не связан ни с каким другим узлом.
+	Node() :pNext(nullptr), pPrev(nullptr) {}
+
+	/*
+	Принимает указатель на предыдущий узел prev и данные ndata, которые будут храниться в узле.
+	Инициализирует поле data данными ndata.
+	Устанавливает указатель pNext текущего узла на следующий узел после prev.
+	Устанавливает указатель pPrev текущего узла на prev.
+	Обновляет указатель pNext предыдущего узла prev, чтобы он указывал на текущий узел.
+	Обновляет указатель pPrev следующего узла после prev, чтобы он указывал на текущий узел.
+	*/
+	Node(Node* prev, const Circle& ndata) :data(ndata) {
+		pNext = prev->pNext;
+		prev->pNext = this;
+		pPrev = prev;
+		pNext->pPrev = this;
+	}
+	~Node() {
+		//Проверяет, существует ли предыдущий узел pPrev.
+		if (pPrev != nullptr)
+			//Если существует, обновляет указатель pNext предыдущего узла, чтобы он указывал на следующий узел после текущего узла pNext.
+			pPrev->pNext = pNext;
+		//Проверяет, существует ли следующий узел pNext.
+		if (pNext != nullptr)
+			//Если существует, обновляет указатель pPrev следующего узла, чтобы он указывал на предыдущий узел перед текущим узлом pPrev.
+			pNext->pPrev = pPrev;
+		//Устанавливает pNext и pPrev в nullptr, чтобы избежать нежелательных ссылок после удаления узла.
+		pNext = nullptr;
+		pPrev = nullptr;
+	}
+	friend class List;
+	friend ostream& operator <<(ostream&, const List&);
+	friend istream& operator >>(istream& in, List& l);
+};
+
+class List {
+	Node Head; // Указатель на начало списка.
+	Node Tail; //Указатель на конец списка.
+	int size; //Переменная для отслеживания размера списка.
+public:
+	//Конструктор по умолчанию инициализирует Head так, чтобы он указывал на Tail, и Tail так, чтобы он указывал на Head. 
+	// Размер списка устанавливается в 0.
+	List() { 
+		Head.pNext = &Tail;
+		Tail.pPrev = &Head;
+		size = 0;
+	}
+
+	//Добавляет новый узел с данными data в начало списка.
+	void AddToHead(const Circle& data) {
+		new Node(&Head, data);
+		size++;
+	}
+
+	//Добавляет новый узел с данными data в конец списка.
+	void AddToTail(const Circle& data) {
+		new Node(Tail.pPrev, data);
+		size++;
+	}
+
+	//Удаляет первый узел, содержащий данные data, из списка.
+	bool RemoveFirst(const Circle& data) {
+		Node* ptr = Head.pNext;
+		for (int i = 0; i < size; i++) {
+			if (ptr->data == data) {
+				delete ptr;
+				size--;
+				return true;
+			}
+			ptr = ptr->pNext;
+		}
+		return false;
+	}
+
+	//Удаляет все узлы, содержащие данные data, из списка и возвращает количество удаленных узлов.
+	int RemoveAllCount(const Circle& data) {
+		int removes = 0;
+		Node* ptr = Head.pNext;
+		for (int i = 0; i < size; i++) {
+			if (ptr->data == data) {
+				Node* tmp = ptr->pNext;
+				delete ptr;
+				removes++;
+				ptr = tmp;
+			}
+			else
+				ptr = ptr->pNext;
+		}
+		size -= removes;
+		return removes;
+	}
+
+	// Очищает список, освобождая память, занятую узлами.
+	void Clear() {
+		Node* ptr = Head.pNext;
+		for (int i = 0; i < size; i++) {
+			Node* tmp = ptr->pNext;
+			delete ptr;
+			ptr = tmp;
+		}
+		size = 0;
+		/*if ((Head.pNext == &Tail) && (Tail.pPrev == &Head))
+			std::cout << "Done!!!\n";*/
+	}
+
+	//Сортирует список в порядке убывания значений площадей фигур, используя метод сортировки "пузырьком".
+	void sort() {
+		if (size > 1) {
+			Node* current = nullptr;
+			Node* nextcurrent = nullptr;
+			for (current = Head.pNext; current->pNext != &Tail; current = current->pNext) {
+				for (nextcurrent = current->pNext; nextcurrent != &Tail; nextcurrent = nextcurrent->pNext) {
+					if (current->data.GetS() < nextcurrent->data.GetS()) {
+						Circle temp = current->data;
+						current->data = nextcurrent->data;
+						nextcurrent->data = temp;
+					}
+				}
+			}
+		}
+	}
+	friend ostream& operator <<(ostream&, const List&);
+	friend istream& operator >>(istream& in, List& l);
 };
--- Lab2s_4/main.cpp
+++ Lab2s_4/main.cpp
@@ -1,13 +1,98 @@
 #include <iostream>
 #include <string.h>
 #include "Circle.cpp"
+#include<fstream>
 using namespace std;
+
+ostream& operator <<(ostream& out, const Point& p) {
+	out << "(" << p.x << ";" << p.y << ")";
+	return out;
+}
+istream& operator >>(istream& in, Point& p) {
+	in.ignore(1);
+	in >> p.x;
+	in.ignore(1);
+	in >> p.y;
+	in.ignore(1);
+	return in;
+}
+ostream& operator <<(ostream& out, const Circle& c) {
+	out << "Площадь: " << c.GetS() << "\tрадиус: " << c.radius << "\tкоординаты: " << c.centr << "\n";
+	return out;
+}
+istream& operator >>(istream& in, Circle& c) {
+	in.ignore(strlen("Площадь: "));
+	double s;
+	in >> s;
+	in.ignore(strlen("\tрадиус: "));
+	in >> c.radius;
+	in.ignore(strlen("\tкоординаты: "));
+	in >> c.centr;
+	return in;
+}
+ostream& operator <<(ostream& out, const List& l) {
+	Node* ptr = l.Head.pNext;
+	for (int i = 0; i < l.size; i++) {
+		out << ptr->data;
+		ptr = ptr->pNext;
+	}
+	return out;
+}
+istream& operator >>(istream& in, List& l) {
+	Circle addData;
+	while (in >> addData) {
+		l.AddToTail(addData);
+	}
+	return in;
+}
+
 
 int main() 
 {
 	setlocale(LC_ALL, "rus");
-	Circle c;
-	c.print();
-	Circle cc(Point(1, 3), 5);
-	cc.print();
+	Point c1(8, 2);
+	Point c2(10, 1);
+	Point c3;
+	Point c4(100, 200);
+	Circle circle1(c1, 3);
+	Circle circle2(c2, 5);
+	Circle circle3(c3, 0);
+	Circle circle4(c4, 1);
+	List l; //Создается объект класса List для хранения кругов.
+
+	//Круги добавляются в список l с использованием методов AddToHead() и AddToTail().
+	l.AddToHead(circle1);
+	l.AddToTail(circle2);
+	l.AddToTail(circle3);
+	l.AddToTail(circle4);
+	l.AddToTail(circle2);
+
+	//Вызывается метод sort() списка l, который сортирует круги в порядке убывания их площадей.
+	l.sort();
+	
+	//Создается файл "res.txt", в который записывается содержимое списка l.
+	ofstream file;
+	file.open("res.txt");
+	if (!file)
+		cout << "Не найден";
+	else {
+		file << l;
+	}
+	file.close();
+
+	//Список l очищается.
+	l.Clear();
+
+	//Из файла "res.txt" считывается содержимое и загружается в список l.
+	ifstream ifile("res.txt");
+	if (!ifile)
+		cout << "Не найден";
+	else {
+		ifile >> l;
+	}
+	ifile.close();
+
+	//Содержимое списка l выводится на экран.
+	cout << l;
+	return 0;
 }
